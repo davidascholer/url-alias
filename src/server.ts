ca0cb85fd236b46c4b6264234c9a4a377934d1b5
David Scholer <davidascholer@gmail.com>
2025-06-23 13:35:51 -0700

    file system write function moved to utils

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,13 @@
 // Create a basic Express server with TypeScript that simply returns "Hello, World!" on the root path.
 import express from "express";
-import fs from "fs/promises";
-import logger from "./logging";
+import { saveData } from "./util";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const DATA_FILE = "./data.json";
 
-// Save to file
-async function saveData(data: { "message": string }): Promise<boolean> {
-  try {
-    await fs.writeFile(DATA_FILE, JSON.stringify(data, null, 2));
-    return true;
-  } catch (error) {
-    logger("Error saving data: " + error, "error");
-    return false;
-  }
-}
-
 app.get("/", async (req, res) => {
-  const dataCreated = await saveData({ message: "Hello, World!" });
+  const dataCreated = await saveData(DATA_FILE, { message: "Hello, World!" });
   res.send({ message: "data created successfully" });
 });
 
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,11 @@
+import fs from "fs/promises";
+import logger from "./logging";
+
+/**
+ * Creates a random alphanumeric string of a specified length.
+ * @param len number of characters to output
+ * @returns string with a length of the input parameter
+ */
 export function getRandomAlphanumeric(len: number) {
   const alphaNumericChars =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -7,4 +15,19 @@ export function getRandomAlphanumeric(len: number) {
     randomString.push(alphaNumericChars.charAt(randomIndex));
   }
   return randomString.join("");
+}
+
+/**
+ * Saves a JSON object to a file.
+ * @param data json object
+ * @returns whether or not the operation was successful
+ */
+export async function saveData(fileName: string, data: { "message": string }): Promise<boolean> {
+  try {
+    await fs.writeFile(fileName, JSON.stringify(data, null, 2));
+    return true;
+  } catch (error) {
+    logger("Error saving data: " + error, "error");
+    return false;
+  }
 }
